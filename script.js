287414dd778915f07c92ba1feebd90645266b0ec
Tejasvi Raghuvanshi <tejasviraghuvanshi@gmail.com>
2023-01-27 21:15:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,16 +15,18 @@ const getWeatherData = (city) => {
 	.then(response => {
 		console.log(response);
 		temp.innerHTML = response.temp;
+		temp2.innerHTML = response.temp;
 		feels_like.innerHTML = response.feels_like;
 		humidity.innerHTML = response.humidity;
-		max_temp.innerHTML = response.max_temp;
+		humidity2.innerHTML = response.humidity;
+		max_temp.innerHTML = response.max_temp;		
 		min_temp.innerHTML = response.min_temp;
-		cloud_pct.innerHTML = response.cloud_pct;
+	//	cloud_pct.innerHTML = response.cloud_pct;
 		sunrise.innerHTML = response.sunrise;
 		sunset.innerHTML = response.sunset;
-		temp.innerHTML = response.temp;
 		wind_degrees.innerHTML = response.wind_degrees;
 		wind_speed.innerHTML = response.wind_speed;
+		wind_speed2.innerHTML = response.wind_speed;
 	})
 	.catch(err => console.error(err));
 }
